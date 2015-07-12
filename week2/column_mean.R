84f0a2e856b0d6f691466a21881d23b1b7cdf98e
Wayne Lewis <wlewis@bnl.gov>
2015-07-12 18:49:00 -0400

    Add option to remove NA values

--- week2/column_mean.R
+++ week2/column_mean.R
@@ -1,10 +1,10 @@
 # Calculate means of each column
 
-column_mean <- function(y) {
+column_mean <- function(y, removeNA = TRUE) {
     nc <- ncol(y)
     means <-numeric(nc)
     for (i in 1:nc) {
-        means[i] <- mean(y[,i])
+        means[i] <- mean(y[,i], na.rm = removeNA)
     }
-    return means
+    return(means)
 }
