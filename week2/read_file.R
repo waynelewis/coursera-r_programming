ac7594ab34bf26d8f1426924909491a875006e86
Wayne Lewis <waynelewis@fastmail.fm>
2015-07-17 22:36:05 -0400

    Allow passing in full file name as well as index number

--- week2/read_file.R
+++ week2/read_file.R
@@ -1,6 +1,15 @@
-read_file <- function(file_id = 1, directory = ".") {
-    file = paste(c(sprintf("%03d",file_id), ".csv"), collapse='')
-    path = paste(c(directory, "/", file), collapse='')
-    data <-read.csv(file = path, header=TRUE)
+# Week 2, Part 1
+# 2015-07-15
+
+read_file <- function(directory = ".", file_id = FALSE, file_name=FALSE) {
+    if (file_id != FALSE) {
+        file <- paste(c(sprintf("%03d",file_id), ".csv"), collapse='')
+    }
+    else {
+        file <- file_name
+    }
+
+        path <- paste(c(directory, "/", file), collapse='')
+        data <-read.csv(file = path, header=TRUE)
 }
 
