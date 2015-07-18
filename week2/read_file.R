ebdd6665dad32def3b0f2165ef53cbff72cb161f
Wayne Lewis <waynelewis@fastmail.fm>
2015-07-17 21:24:15 -0400

    Add directory argument

--- week2/read_file.R
+++ week2/read_file.R
@@ -1,4 +1,4 @@
-read_file <- function(file_id = 1) {
+read_file <- function(file_id = 1, directory = ".") {
     file = paste(c(sprintf("%03d",file_id), ".csv"), collapse='')
     path = paste(c(directory, "/", file), collapse='')
     data <-read.csv(file = path, header=TRUE)
