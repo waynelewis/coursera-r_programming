e1ae8cd2b7d5bd245543851510ff89629fee9c74
Wayne Lewis <waynelewis@fastmail.fm>
2015-07-17 21:24:37 -0400

    Initial commit of working function

---
+++ week2/complete.R
@@ -0,0 +1,38 @@
+complete <- function(directory, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+
+    ## Return a data frame of the form:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+
+    source("read_file.R")
+    
+    df <- data.frame(matrix(ncol = 2, nrow = length(id)))
+    colnames(df) <- c("id", "nobs")
+
+    index <- 1
+
+    for (file_id in id) {
+        data <- read_file(directory = directory, file_id)
+        nobs <- find_complete_observations(data)
+        df[index,] = c(file_id, nobs)
+        index <- index + 1 
+    }
+    df
+}
+
+find_complete_observations <- function(data) {
+    sulf_ok <- !is.na( data[,2])
+    nit_ok <- !is.na( data[,3])
+    both_ok <- sum(sulf_ok & nit_ok)
+}
+
+
